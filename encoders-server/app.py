7ec0a074ad4b66d360da6ef9300ce47434b8e9bd
@saavedramarcosdavid <saavedramarcosdavid@gmail.com>
2021-12-09 17:26:37 -0300

    Creacion de una ruta para el POST de actualizacion de parametros de entrenamiento

--- encoders-server/app.py
+++ encoders-server/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, g
 from flask_sqlalchemy import SQLAlchemy  
 import jyserver.Flask as jsf
 import numpy as np
@@ -58,6 +58,12 @@ class App:
 		'''
 		self.Q = QLearning(self)
 
+		# Inicialización de las variables utilizadas para la administración de alertas en la vista
+		self.post_params = False
+		self.alertas = []
+		self.check = 0
+
+		# Cargado o inicialización de la base de datos
 		if "qtable" in db.inspect(db.engine).get_table_names(): # Si la tabla existe en la base de datos
 			database = QTable.query.all()
 
@@ -73,7 +79,6 @@ class App:
 				self.entrada_db = QTable(q_table=self.Q.q_table)
 				db.session.add(self.entrada_db)
 				db.session.commit()
-
 		else:						# Si la tabla no existe en la base de datos
 			db.create_all()
 			self.Q.inicializar_q_table()
@@ -134,99 +139,16 @@ class App:
 		self.js.update_table(list(q_table.flatten()), list(self.Q.robot.reset()))
 
 
-@app.route('/', methods=['GET','POST'])
+@app.route('/', methods=['GET'])
 def index():
 	'''
 		Metodo correspondiente a la ruta "/" de la web.
-		Se encarga de la gestión del formulario de actualización de los parámetros de entrenamiento.
-			Si esta ruta es accedida mediante el método GET, muestra normalmente el contenido del index
-			En cambio, si se realiza mediante el método POST, además actualiza los parámetros de entrenamiento
-			(utilizando los valores del	formulario o seteando los valores por defecto según se indique)
-			Realiza validaciones de valores máximos y mínimos de estos parámetros
-		Además envía algunos parámetros a la vista como la tabla Q inicial y los parámetros iniciales.
+		Envía algunos parámetros a la vista como la tabla Q inicial,
+		los parámetros de entrenamiento, las alertas y las constantes definidas.
 	'''
-	App.detener()
-	check = 0
-	alertas = []
-	if request.method == 'POST':
-		if 'aplicar' in request.form: # Si se ha clickeado en el botón aplicar
+	App.detener() # Si hubiese quedado en movimiento el Robot, el mismo es detenido
 
-			# Se reciben los parámetros del modelo desde el formulario
-			learning_rate = float(request.form['learning_rate'])
-			discount_factor = float(request.form['discount_factor'])
-			epsilon = float(request.form['epsilon'])
-			learning_epsilon = float(request.form['learning_epsilon'])
-			min_epsilon = float(request.form['min_epsilon'])
-			max_movements = int(request.form['max_movements'])
-			win_reward = int(request.form['win_reward'])
-			loss_reward = int(request.form['loss_reward'])
-			dead_reward = int(request.form['dead_reward'])
-			loop_reward = int(request.form['loop_reward'])
-
-			# Checkeo de los valores mínimos y máximos recibidos, crea alertas
-			check = 1
-			i = 0
-
-			if not(learning_rate>const.minimos[i] and learning_rate<const.maximos[i]):
-				alertas.append(f"Learning Rate fuera de los limites: ({const.minimos[i]}, {const.maximos[i]})")
-				check = -1
-			i+=1
-			if not(discount_factor>const.minimos[i] and discount_factor<const.maximos[i]):
-				alertas.append(f"Discount Factor fuera de los limites: ({const.minimos[i]}, {const.maximos[i]})")
-				check = -1
-			i+=1
-			if not(epsilon>const.minimos[i] and epsilon<const.maximos[i]):
-				alertas.append(f"Epsilon fuera de los limites: ({const.minimos[i]}, {const.maximos[i]})")
-				check = -1
-			i+=1
-			if not(learning_epsilon>const.minimos[i] and learning_epsilon<const.maximos[i]):
-				alertas.append(f"Learning Epsilon fuera de los limites: ({const.minimos[i]}, {const.maximos[i]})")
-				check = -1
-			i+=1
-			if not(min_epsilon>const.minimos[i] and min_epsilon<const.maximos[i]):
-				alertas.append(f"Min Epsilon fuera de los limites: ({const.minimos[i]}, {const.maximos[i]})")
-				check = -1
-			i+=1
-			if not(max_movements>const.minimos[i] and max_movements<const.maximos[i]):
-				alertas.append(f"Max Movements fuera de los limites: ({const.minimos[i]}, {const.maximos[i]})")
-				check = -1
-			i+=1
-			if not(win_reward>const.minimos[i] and win_reward<const.maximos[i]):
-				alertas.append(f"Win Reward fuera de los limites: ({const.minimos[i]}, {const.maximos[i]})")
-				check = -1
-			i+=1
-			if not(loss_reward>const.minimos[i] and loss_reward<const.maximos[i]):
-				alertas.append(f"Loss Reward fuera de los limites: ({const.minimos[i]}, {const.maximos[i]})")
-				check = -1
-			i+=1
-			if not(dead_reward>const.minimos[i] and dead_reward<const.maximos[i]):
-				alertas.append(f"Dead Reward fuera de los limites: ({const.minimos[i]}, {const.maximos[i]})")
-				check = -1
-			i+=1
-			if not(loop_reward>const.minimos[i] and loop_reward<const.maximos[i]):
-				alertas.append(f"Loop Reward fuera de los limites: ({const.minimos[i]}, {const.maximos[i]})")
-				check = -1
-
-			if check == 1: # Si no hay errores, actualiza los parámetros y envia la anecdota 
-				App.Q.set_params(
-					learning_rate,
-					discount_factor,
-					epsilon,
-					learning_epsilon,
-					min_epsilon,
-					max_movements,
-					win_reward,
-					loss_reward,
-					dead_reward,
-					loop_reward
-				)
-				alertas.append("Parámetros actualizados satisfactoriamente")
-
-		elif 'reset' in request.form: # Si se ha clickeado en el botón resetear
-			App.Q.set_default_params()
-			check = 1
-			alertas.append("Parámetros actualizados satisfactoriamente")
-
+	# Valores a ser enviados a la vista
 	q_table = App.Q.q_table
 	config = App.Q.get_params()
 	state = App.Q.robot.state
@@ -237,14 +159,111 @@ def index():
 		'q_table': list(q_table.flatten()),
 		'config': config,
 		'state': state,
-		'check': check,
-		'alertas': alertas,
+		'check': App.check,
+		'alertas': App.alertas,
 		'minimos': const.minimos,
 		'maximos': const.maximos,
 		'steps': const.steps
 	}
+
+	# Si se han actualizado los parámetros de entrenamiento, limpia las variables de alerta
+	if App.post_params:
+		App.post_params = False
+		App.check = 0
+		App.alertas = []
+
 	return App.render(render_template('index.html', data=data))
 
+@app.route("/actualizar_parametros", methods=["POST"])
+def actualizar_parametros():
+	'''
+		Se encarga de la gestión del formulario de actualización de los parámetros de entrenamiento.
+		Actualiza los parámetros de entrenamiento utilizando los valores del formulario o seteando 
+		los valores por defecto según se indique. 
+		Realiza validaciones de valores máximos y mínimos de estos parámetros y agrega a la lista de 
+		alertas los resultados de la actualización.
+	'''
+	App.post_params = True
+
+	if 'aplicar' in request.form: # Si se ha clickeado en el botón aplicar
+
+		# Se reciben los parámetros del modelo desde el formulario
+		learning_rate = float(request.form['learning_rate'])
+		discount_factor = float(request.form['discount_factor'])
+		epsilon = float(request.form['epsilon'])
+		learning_epsilon = float(request.form['learning_epsilon'])
+		min_epsilon = float(request.form['min_epsilon'])
+		max_movements = int(request.form['max_movements'])
+		win_reward = int(request.form['win_reward'])
+		loss_reward = int(request.form['loss_reward'])
+		dead_reward = int(request.form['dead_reward'])
+		loop_reward = int(request.form['loop_reward'])
+
+		# Checkeo de los valores mínimos y máximos recibidos, crea alertas
+		App.check = 1
+		i = 0
+
+		if not(learning_rate>const.minimos[i] and learning_rate<const.maximos[i]):
+			App.alertas.append(f"Learning Rate fuera de los limites: ({const.minimos[i]}, {const.maximos[i]})")
+			App.check = -1
+		i+=1
+		if not(discount_factor>const.minimos[i] and discount_factor<const.maximos[i]):
+			App.alertas.append(f"Discount Factor fuera de los limites: ({const.minimos[i]}, {const.maximos[i]})")
+			App.check = -1
+		i+=1
+		if not(epsilon>const.minimos[i] and epsilon<const.maximos[i]):
+			App.alertas.append(f"Epsilon fuera de los limites: ({const.minimos[i]}, {const.maximos[i]})")
+			App.check = -1
+		i+=1
+		if not(learning_epsilon>const.minimos[i] and learning_epsilon<const.maximos[i]):
+			App.alertas.append(f"Learning Epsilon fuera de los limites: ({const.minimos[i]}, {const.maximos[i]})")
+			App.check = -1
+		i+=1
+		if not(min_epsilon>const.minimos[i] and min_epsilon<const.maximos[i]):
+			App.alertas.append(f"Min Epsilon fuera de los limites: ({const.minimos[i]}, {const.maximos[i]})")
+			App.check = -1
+		i+=1
+		if not(max_movements>const.minimos[i] and max_movements<const.maximos[i]):
+			App.alertas.append(f"Max Movements fuera de los limites: ({const.minimos[i]}, {const.maximos[i]})")
+			App.check = -1
+		i+=1
+		if not(win_reward>const.minimos[i] and win_reward<const.maximos[i]):
+			App.alertas.append(f"Win Reward fuera de los limites: ({const.minimos[i]}, {const.maximos[i]})")
+			App.check = -1
+		i+=1
+		if not(loss_reward>const.minimos[i] and loss_reward<const.maximos[i]):
+			App.alertas.append(f"Loss Reward fuera de los limites: ({const.minimos[i]}, {const.maximos[i]})")
+			App.check = -1
+		i+=1
+		if not(dead_reward>const.minimos[i] and dead_reward<const.maximos[i]):
+			App.alertas.append(f"Dead Reward fuera de los limites: ({const.minimos[i]}, {const.maximos[i]})")
+			App.check = -1
+		i+=1
+		if not(loop_reward>const.minimos[i] and loop_reward<const.maximos[i]):
+			App.alertas.append(f"Loop Reward fuera de los limites: ({const.minimos[i]}, {const.maximos[i]})")
+			App.check = -1
+
+		if App.check == 1: # Si no hay errores, actualiza los parámetros y envia la anecdota 
+			App.Q.set_params(
+				learning_rate,
+				discount_factor,
+				epsilon,
+				learning_epsilon,
+				min_epsilon,
+				max_movements,
+				win_reward,
+				loss_reward,
+				dead_reward,
+				loop_reward
+			)
+			App.alertas.append("Parámetros actualizados satisfactoriamente")
+
+	elif 'reset' in request.form: # Si se ha clickeado en el botón resetear
+		App.Q.set_default_params()
+		App.check = 1
+		App.alertas.append("Parámetros actualizados satisfactoriamente")
+
+	return redirect(url_for('index'))
 
 def pagina_no_encontrada(error):
 	'''
